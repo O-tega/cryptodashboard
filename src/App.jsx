6388b6b02a2e21116e84250233ee6aae01d4e6e8
O-tega <tega.myview@gmail.com>
2023-06-24 13:44:24 +0100

    create a react app with vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+function App() {
+  return (
+    <>
+      <p className='justify-center h-screen flex items-center text-3xl text-blue-400 font-bold'>This is a new react App</p>
+    </>
+  )
+}
+
+export default App
+
